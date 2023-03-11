343f0a34c61a2c173a9eed8f6ffbaef280713401
Jisoo Ahn <70849122+An-jisu@users.noreply.github.com>
2023-03-11 18:21:35 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    my_string = my_string.split(' ')
+    result = int(my_string[0])
+    
+    for i in range(1, len(my_string),2):
+        if my_string[i]=='+':
+            result += int(my_string[i+1])
+        elif my_string[i]=='-':
+            result -= int(my_string[i+1])
+    return result
